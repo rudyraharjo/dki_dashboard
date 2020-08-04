1a92a2eff518653b9707dda3019e67c1833d4522
rudyraharjo <rudy.raharjo@danakini.co.id>
2020-08-04 09:16:54 +0700

    update list report

--- src/app/datareport.service.ts
+++ src/app/datareport.service.ts
@@ -19,21 +19,26 @@ export class DatareportService {
     },
     {
       id: 1,
-      name: "Report Realtime Sales Monitoring",
-      link: "https://datastudio.google.com/embed/reporting/1530f3ec-bc1f-4bbb-8c91-86836a770d55/page/y22TB"
+      name: "Report Real-time Sales Monitoring - Approval",
+      link: "https://datastudio.google.com/embed/reporting/e6cba33b-7137-488d-b084-37438335657e/page/y22TB"
     },
     {
       id: 2,
+      name: "Report Realtime Sales Monitoring - Confirmed and Disbursed",
+      link: "https://datastudio.google.com/embed/reporting/1530f3ec-bc1f-4bbb-8c91-86836a770d55/page/y22TB"
+    },
+    {
+      id: 3,
       name: "Report Performance Trainer Danakini",
       link: "https://datastudio.google.com/embed/reporting/9a215ff0-e617-4f64-969d-6dba2e0e8b2d/page/eTAVB"
     },
     {
-      id: 3,
+      id: 4,
       name: "Report Loan Portfolio Management",
       link: "https://datastudio.google.com/embed/reporting/ff90b676-8e31-4605-b438-dff9120a7729/page/3U7WB"
     },
     {
-      id: 4,
+      id: 5,
       name: "Report Loan Origination System",
       link: "https://datastudio.google.com/embed/reporting/e56ffac1-2be6-4440-be98-2b3db29ea5c8/page/qZLXB"
     }
